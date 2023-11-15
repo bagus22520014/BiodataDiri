5d3a6a2588fd722fbbdd277ff957c1d72a792a52
bagus22520014 <baguspw17@gmail.com>
2023-11-15 18:49:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/biodatadiri/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.biodatadiri
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.biodatadiri.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnSave.setOnClickListener { goToProfilActivity() }
+    }
+
+    private fun goToProfilActivity() {
+        val intent = Intent (this, ProfilActivity::class.java)
+        startActivity(intent)
+    }
+}
